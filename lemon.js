2aeb413fbf61be1e5d482ff9c7b18a633edec03f
maanya125 <83495427+maanya125@users.noreply.github.com>
2024-07-20 20:06:53 +0530

    Update lemon.js

--- lemon.js
+++ lemon.js
@@ -13,10 +13,65 @@ function modifyPayload(payload) {
   // Decode the payload
   let decodedPayload = JSON.parse(base64Decode(payload));
 
-  // Modify some fields
-  decodedPayload.profileStatusReason = 1000;  // Modify the profileStatusReason field
-  decodedPayload.userProfile.firstName = "John";  // Modify the firstName field
-  decodedPayload.userProfile.lastName = "Doe";  // Modify the lastName field
+  // Update the specified fields
+  decodedPayload.responseType = "INITIAL";
+  decodedPayload.profileStatus = "PROFILE_AVAILABLE";
+  decodedPayload.profileStatusReason = 1000;
+  decodedPayload.profileStatusReasonText = "Profile Available due to an acquired plan provisioned and ACTIVE";
+  decodedPayload.appLicenseMode = "PAID";
+  decodedPayload.appProfile = {
+    "appId": "RocketIOS1",
+    "accessibleItems": [
+      {
+        "status": "ACTIVE",
+        "source": {
+          "owner_id": "1E55127C60B4F9040A495C79@AdobeID",
+          "id": "0B10E44A1414D2DC572A",
+          "type": "LICENSE",
+          "status_reason": "NORMAL",
+          "can_access_until": 1725544243292
+        },
+        "fulfillable_items": {
+          "cc_storage": {
+            "enabled": true,
+            "feature_sets": {
+              "CS_LVL_2": {
+                "id": "CS_LVL_2",
+                "label": "CS LVL 2",
+                "enabled": true
+              },
+              "VRT_30": {
+                "id": "VRT_30",
+                "label": "VRT 30",
+                "enabled": true
+              }
+            },
+            "charging_model": {
+              "cap": 100,
+              "unit": "GB",
+              "model": "RECURRING",
+              "overage": "NA"
+            }
+          },
+          "core_services_cc": {
+            "enabled": true,
+            "feature_sets": {
+              "CS_LVL_2": {
+                "id": "CS_LVL_2",
+                "label": "CS LVL 2",
+                "enabled": true
+              }
+            },
+            "charging_model": {
+              "model": "RECURRING",
+              "overage": "NA",
+              "rollover": 0
+            }
+          }
+        }
+      }
+    ]
+  };
 
   // Encode the modified payload back to base64
   let modifiedPayload = base64Encode(JSON.stringify(decodedPayload));
@@ -29,7 +84,7 @@ function modifyResponse(response) {
   // Parse the JSON response
   let jsonResponse = JSON.parse(response);
 
-  // Decode, modify and encode the payload
+  // Decode, modify, and encode the payload
   jsonResponse.asnp.payload = modifyPayload(jsonResponse.asnp.payload);
 
   // Convert the modified JSON back to string
